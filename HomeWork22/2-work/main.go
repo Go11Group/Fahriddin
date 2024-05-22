3968a4e72a584eab2b038ba4d6717e30492a8eeb
fahriddin <fahriddinrahimberdiyev@gmail.com>
2024-05-22 18:03:59 +0500

    Change feature-1

--- HomeWork22/2-work/main.go
+++ HomeWork22/2-work/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	n := 10
+	n := []int{1, 2, 3, 4, 5, 7, 6, 9}
 	Printer(n)
 }
 
-func Printer(n int) {
-	for i := 0; i < n; i++ {
-		fmt.Println(i)
+func Printer(n []int) {
+	for i := 0; i < len(n); i++ {
+		fmt.Println(n[i])
 	}
 }
