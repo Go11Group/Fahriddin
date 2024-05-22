8f13e48bb4f0070643c7a1f8e455e354e0c724cf
fahriddin <fahriddinrahimberdiyev@gmail.com>
2024-05-22 16:08:30 +0500

    changed

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	n := "Salom"
+	n := 10
 	Printer(n)
 }
 
-func Printer(n string) {
-	for i := 0; i < len(n); i++ {
+func Printer(n int) {
+	for i := 0; i < n; i++ {
 		fmt.Println(i)
 	}
 }
