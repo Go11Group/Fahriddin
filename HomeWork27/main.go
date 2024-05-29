af920ca6354d631ae044ff0da7a4520ed2a29be8
fahriddin <fahriddinrahimberdiyev@gmail.com>
2024-05-29 15:24:51 +0500

    Full home work

--- HomeWork27/main.go
+++ HomeWork27/main.go
@@ -14,6 +14,13 @@ type Cars struct {
 	Price int
 }
 
+type Join struct {
+	Name1 string
+	Name2 string
+	Year  int
+	Price int
+}
+
 func main() {
 	db, err := sql.Open("postgres", "host = localhost port = 5432 user = postgres dbname = nt password = 0412")
 
@@ -73,5 +80,46 @@ func main() {
 	// 	}
 	// }
 	// fmt.Println("10 new cars added successfully!")
+
+	// InsertQueary = "Insert into Users(Name, Car_id) Values($1, $2)"
+
+	// Users := []struct {
+	// 	Name   string
+	// 	Car_id string
+	// }{
+	// 	{"Jamshid", "daba7966-8de2-4ec5-804e-1e7111b07ebb"},
+	// 	{"Ali", "b60c9833-decc-4004-aa1f-06da7f46b450"},
+	// 	{"Sarvar", "00053c5b-0b18-430a-a878-fe19fce64129"},
+	// 	{"Muhriddin", "6e917376-00e7-4113-bb2f-d09f5253ef17"},
+	// 	{"Bekzod", "9be214de-3de1-4b8f-9bd5-1fadff6d9207"},
+	// 	{"Nurmuhammad", "01e6a6d6-7ba3-437e-bbfd-c02af8443fd1"},
+	// 	{"Sanjarbek", "99f17925-43f3-431e-9d88-cf28aa7f1e37"},
+	// 	{"Hamidjon", "3f8278ee-e3cf-472a-9859-9bc0cd41c335"},
+	// 	{"Diyorbek", "1145b367-9d1d-4982-b331-2ed6de82f294"},
+	// 	{"Fahriddin", "be034c81-863c-4663-9ba7-90460f8c80d3"},
+	// }
+
+	// for _, user := range Users {
+	// 	_, err = db.Exec(InsertQueary, user.Name, user.Car_id)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("10 new users added successfully!")
+
+	// rows, err := db.Query(`Select U.name, C.Brand, C.year, C.price from Users as U 
+	// Join Cars as C
+	// On C.id = U.car_id`)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// j := Join{}
+	// for rows.Next() {
+	// 	err = rows.Scan(&j.Name1, &j.Name2, &j.Year, &j.Price)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(j)
+	// }
 
 }
--- HomeWork27/work.sql
+++ HomeWork27/work.sql
@@ -4,3 +4,9 @@ Create Table Cars(Id uuid Primary Key not null default gen_random_uuid(),
     Price int
     );
 
+Create Table Users(Id uuid Primary Key not null default gen_random_uuid(),
+Name varchar,
+Car_id uuid not null ,
+Foreign Key (Car_id) References Cars(id)
+);
+
