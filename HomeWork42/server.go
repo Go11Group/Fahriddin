2273ce1b8ad5a824601ccbb575ff11eb17e4fa49
fahriddin <fahriddinrahimberdiyev@gmail.com>
2024-06-21 14:31:21 +0500

    FUll Home Work

--- HomeWork42/server.go
+++ HomeWork42/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -11,9 +12,41 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// URL ni olish
 	url := r.URL.String()
 	fmt.Println("Received URL:", url)
+
 	// URL ni qaytarish (optional)
 	fmt.Fprintf(w, "URL: %s", url)
 
+	client := http.Client{}
+
+	switch r.Method {
+	case "GET":
+		res := Get(url)
+		fmt.Fprintf(w, res)
+	case "POST":
+		str, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+		res := Post(str, url, &client)
+		fmt.Fprint(w, res)
+	case "PUT":
+		str, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+		Put(&client, str, url)
+	case "DELETE":
+		str,err := io.ReadAll(r.Body)
+		if err != nil{
+			panic(err)
+		}
+		Delete(&client,url,str)
+	}
+}
+
+func Get(url string) string {
 	URL := "http://localhost:8080" + url
 
 	resp, err := http.Get(URL)
@@ -23,7 +56,50 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	body, _ := io.ReadAll(resp.Body)
-	fmt.Fprintf(w,string(body))
+	return string(body)
+}
+
+func Post(str []byte, url string, client *http.Client) string {
+	URL := "http://localhost:8080" + url
+	resp, err := http.Post(URL, "application/json", bytes.NewBuffer(str))
+	if err != nil {
+		panic(err)
+	}
+	res, _ := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(res)
+}
+
+func Put(client *http.Client, str []byte, url string){
+	URL := "http://localhost:8080" + url
+	req, err := http.NewRequest("PUT", URL, bytes.NewBuffer(str))
+
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/text")
+	_,err = client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Delete(client *http.Client,url string,str []byte){
+	URL := "http://localhost:8080" + url
+	req,err := http.NewRequest("DELETE",URL,bytes.NewBuffer(str))
+
+	if err != nil{
+		panic(err)
+	}
+	req.Header.Set("Content-Type","application/json")
+	_,err = client.Do(req)
+
+	if err != nil{
+		panic(err)
+	}
 }
 
 func main() {
